d941469984a9f4602770397f1a7baef94c1a388e
Niraj Thanki <nirajthanki6@gmail.com>
2017-01-08 13:36:14 -0800

    Add files via upload

---
+++ Macro_Functions.cpp
@@ -0,0 +1,22 @@
+/*****************************************************************************
+ *  Northwestern Polytechnic University
+ *  CS360 - c++
+ *****************************************************************************/
+/*****************************************************************************
+ *  main.c
+ *
+ *  Description of this source file.
+ *
+ *  Author:  THANKI NIRAJ and 19376
+ *****************************************************************************/
+ 
+#include <stdio.h> 
+#define SumOf(x,y) ((x)+(y))
+int main() { 
+int arg1=1;
+int arg2=7; 
+printf("\narg1=%i,arg2=%i",arg1,arg2); 
+printf("\nSumOf(arg1,arg2)=%i",SumOf(arg1,arg2)); 
+printf("\n2*SumOf(arg1,arg2)=%i",2*SumOf(arg1,arg2)); 
+} 
+
